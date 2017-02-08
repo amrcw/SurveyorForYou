8bf817ac2626713b94e2a9041cbbc8a1351edcb8
amrcw <amrcw_rish24@yahoo.co.uk>
2017-02-08 17:31:45 +0000

    Registration Database

--- AndroidStudioProjects/SurveyorForYou/app/src/main/java/uk/co/surveyorforyou/surveyorforyou/RegisterActivity.java
+++ AndroidStudioProjects/SurveyorForYou/app/src/main/java/uk/co/surveyorforyou/surveyorforyou/RegisterActivity.java
@@ -1,10 +1,20 @@
 package uk.co.surveyorforyou.surveyorforyou;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -14,10 +24,61 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final EditText rgname = (EditText)findViewById(R.id.regName);
-        final EditText rgemail = (EditText)findViewById(R.id.regEmail);
-        final EditText rguser = (EditText)findViewById(R.id.regUsername);
-        final EditText rgpass = (EditText)findViewById(R.id.regPass);
-        final Button login = (Button)findViewById(R.id.btnReg);
+        final EditText rgFname = (EditText)findViewById(R.id.regFirstname);
+        final EditText rgLname = (EditText)findViewById(R.id.regLastname);
+        final EditText rgEmail = (EditText)findViewById(R.id.regEmail);
+        final EditText rgUser = (EditText)findViewById(R.id.regUsername);
+        final EditText rgPass = (EditText)findViewById(R.id.regPassword);
+        final EditText rgConfirmpass = (EditText)findViewById(R.id.regConfirmpassword);
+        final EditText rgAddress = (EditText)findViewById(R.id.regAddress);
+        final EditText rgPostcode = (EditText)findViewById(R.id.regPostcode);
+        final Button register = (Button)findViewById(R.id.btnReg);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final  String firstname = rgFname.getText().toString();
+                final  String lastname = rgLname.getText().toString();
+                final  String email = rgEmail.getText().toString();
+                final  String username = rgUser.getText().toString();
+                final  String password = rgPass.getText().toString();
+                final  String address = rgAddress.getText().toString();
+                final  String postcode = rgPostcode.getText().toString();
+
+                Response.Listener<String> responseListner = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+                                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                RegisterRequest rgRequest = new RegisterRequest(firstname,lastname,email,username,password,address,postcode,responseListner);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(rgRequest);
+            }
+        });
+
+
+
     }
 }
---
+++ AndroidStudioProjects/SurveyorForYou/app/src/main/java/uk/co/surveyorforyou/surveyorforyou/RegisterRequest.java
@@ -0,0 +1,35 @@
+package uk.co.surveyorforyou.surveyorforyou;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Ruwan on 08/02/2017.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URI = "http://www.surveyor-for-you.co.uk/Android/register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String firstname, String lastname, String email, String username, String password, String address, String postcode, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URI, listener, null);
+
+        params = new HashMap<>();
+        params.put("firstname", firstname);
+        params.put("lastname", lastname);
+        params.put("email", email);
+        params.put("username",username);
+        params.put("password",password);
+        params.put("address",address);
+        params.put("postcode",postcode);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
