80e62bd5e0e6c85325c137fc4cec6174405ec967
amrcw <amrcw_rish24@yahoo.co.uk>
2017-02-20 18:39:20 +0000

    Search Listview

--- AndroidStudioProjects/SurveyorForYou/app/src/main/java/uk/co/surveyorforyou/surveyorforyou/PropertiesAdapter.java
+++ AndroidStudioProjects/SurveyorForYou/app/src/main/java/uk/co/surveyorforyou/surveyorforyou/PropertiesAdapter.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,19 +18,25 @@ import java.util.List;
  * Created by Ruwan on 17/02/2017.
  */
 
-public class PropertiesAdapter extends ArrayAdapter{
+public class PropertiesAdapter extends ArrayAdapter implements Filterable{
 
-    List list = new ArrayList();
+    List<Properties> list = new ArrayList();
+    CustomFilter customFilter;
+
+
 
 
     public PropertiesAdapter(PropertiesFragment context, int resource) {
         super(context.getContext(), resource);
     }
 
+
+
+
     @Override
     public void add(Object object) {
        super.add(object);
-        list.add(object);
+        list.add((Properties) object);
     }
 
     @Override
@@ -57,7 +65,7 @@ public class PropertiesAdapter extends ArrayAdapter{
             propertiesHolder.name = (TextView) row.findViewById(R.id.clientName);
             propertiesHolder.phone = (TextView) row.findViewById(R.id.clientPhone);
             propertiesHolder.postcode = (TextView) row.findViewById(R.id.surveyPostcode);
-            propertiesHolder.dateOrdered = (TextView) row.findViewById(R.id.dateOrdered);
+           propertiesHolder.dateOrdered = (TextView) row.findViewById(R.id.dateOrdered);
             propertiesHolder.dueDate = (TextView) row.findViewById(R.id.dateDue);
             row.setTag(propertiesHolder);
         }else{
@@ -79,5 +87,43 @@ public class PropertiesAdapter extends ArrayAdapter{
     static class PropertiesHolder{
 
         TextView refNo,name,phone,postcode,dateOrdered,dueDate;
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return super.getFilter();
+    }
+
+    class CustomFilter extends Filter{
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            FilterResults filterResults = new FilterResults();
+
+            if(constraint != null && constraint.length() > 0){
+
+                constraint = constraint.toString().toUpperCase();
+                ArrayList<Properties> filer = new ArrayList<>();
+
+                for (int i=0; i< list.size(); i++){
+                    if(list.get(i).getRefNo().toUpperCase().contains(constraint)){
+                        Properties p = new Properties(list.get(i).getRefNo(),list.get(i).getName(),list.get(i).getPhone(),list.get(i).getPostcode(),list.get(i).getDateOrdered(),list.get(i).getDueDate());
+                        filer.add(p);
+                    }
+                }
+
+                filterResults.count = filer.size();
+                filterResults.values = list;
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+        }
     }
 }
